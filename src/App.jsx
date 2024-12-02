87afda9828ab830638f58e0f3e0b26651e83af86
Your Name <kiranmmm138@gmail.com>
2024-12-02 14:22:53 +0545

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import ThemeProviderWrapper from './components/ThemeProviderWrapper';
 import ResizableRotatableImage from './components/ResizableImage';
 
 const App = () => {
   return (
-    <ThemeProviderWrapper>
       <ResizableRotatableImage />
-    </ThemeProviderWrapper>
+   
   );
 };
 
